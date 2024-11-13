647cd5e98ce862716436ef0a3b0b0bda0114f9a1
Frank Shih <fshih37@gmail.com>
2024-11-12 20:34:36 -0500

    update

--- PINN/pinn_bnn.py
+++ PINN/pinn_bnn.py
@@ -5,35 +5,48 @@ from PINN.common.base_pinn import BasePINN
 from PINN.common.torch_layers import BayesianNN
 
 
+
 class PINN_BNN(BasePINN):
     def __init__(
         self,
         physics_model,
+        dataset,
         hidden_layers=[15, 15],
         activation_fn=nn.Softplus(beta=10),
         lr=1e-3,
         physics_loss_weight=1,
-        kl_weight=1e-1,
         save_path=None,
+        device='cpu'
     ) -> None:
-        super().__init__(physics_model, hidden_layers, activation_fn, lr, physics_loss_weight, save_path)
-        self.kl_loss = bnn.BKLLoss(reduction='mean', last_layer_only=False)
-        self.kl_weight = kl_weight
-        
+        super().__init__(physics_model, dataset, hidden_layers, activation_fn, lr, physics_loss_weight, save_path, device)
+    
+    
 
-    def _pinn_init(self):
-        self.net = BayesianNN(input_dim=self.input_dim, output_dim=self.output_dim, hidden_layers=self.hidden_layers, activation_fn=self.activation_fn)
-        self.optimiser = optim.Adam(self.net.parameters(), lr=self.lr)
+    
+    def pde_loss(self):
+        loss = 0
+        for i, d in enumerate(self.dataset):
+            if d['category'] == 'differential':
+                diff_o = self.differential_operator(self.net, d['X'])
+                loss += self.mse_loss(diff_o, d['y'])
+        return loss
+    
+    def solution_loss(self):
+        loss = 0
+        for i, d in enumerate(self.dataset):
+            if d['category'] == 'solution':
+                loss += self.mse_loss(d['y'], self.net(d['X']))
+        return loss
     
     def update(self):
         self.optimiser.zero_grad()
-        outputs = self.net(self.X)
-        loss = self.mse_loss(self.y, outputs) + self.kl_weight * self.kl_loss(self.net)
-        loss += self.physics_loss_weight * self.physics_loss(self.net)
+        # outputs = self.net(self.X)
+        # loss = self.mse_loss(self.y, outputs)
+        loss = self.solution_loss() + self.physics_loss_weight * self.pde_loss()
+        # loss += self.physics_loss_weight * self.physics_loss(self.net, self.physics_X)
         
         loss.backward()
         self.optimiser.step()
         
         return loss
-
 
