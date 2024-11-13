b90d6562957879573efc014c248cb6c94bb80801
Frank Shih <fshih37@gmail.com>
2024-11-13 12:59:18 -0500

    update

--- PINN/common/base_pinn.py
+++ PINN/common/base_pinn.py
@@ -26,11 +26,11 @@ class BasePINN(object):
         super().__init__()
         self.physics_model = physics_model
         self.dataset = dataset.copy()
-        for key, value in self.physics_model.__dict__.items():
-            setattr(self, key, value)
+        # for key, value in self.physics_model.__dict__.items():
+        #     setattr(self, key, value)
 
         # Physics loss
-        self.physics_loss = self.physics_model.physics_loss
+        # self.physics_loss = self.physics_model.physics_loss
         self.differential_operator = self.physics_model.differential_operator
         self.physics_loss_weight = physics_loss_weight
         
@@ -38,15 +38,13 @@ class BasePINN(object):
         self.lr = lr
         self.hidden_layers = hidden_layers
         self.activation_fn = activation_fn
-        self.mse_loss = nn.MSELoss()
+        self.mse_loss = nn.MSELoss(reduction='mean')
         
         self.save_path = save_path
         self.device = device
         self.physics_model.plot_true_solution(save_path)
 
         # To device
-        # self.X = self.X.to(self.device)
-        # self.y = self.y.to(self.device)
         self.X = torch.cat([d['X'] for d in self.dataset if d['category'] == 'solution'], dim=0).to(self.device)
         self.y = torch.cat([d['y'] for d in self.dataset if d['category'] == 'solution'], dim=0).to(self.device)
                            
@@ -55,10 +53,7 @@ class BasePINN(object):
         
         self.input_dim = self.X.shape[1]
         self.output_dim = self.y.shape[1]
-        # self.physics_X = self.physics_X.to(self.device)
-        
-        # self._pinn_init()
-        # self.net.to(self.device)
+
 
     def _pinn_init(self):
         # init pinn net and optimiser
