82113a2c367537ae1011967b5da670109e44191f
zhenghaojiang <hzyhjzh@gmail.com>
2024-11-16 21:31:19 -0500

    update bpinn into train()

--- PINN/__init__.py
+++ PINN/__init__.py
@@ -4,3 +4,4 @@ from PINN.pinn_efi import PINN_EFI
 from PINN.pinn_bnn import PINN_BNN
 from PINN.pretrain_efi import Pretrain_EFI
 from PINN.pinn_efi_discovery import PINN_EFI_Discovery
+from PINN.bpinn_hmc import BayesianPINN
