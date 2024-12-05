36b6a57620d76d497028bbf713a9840a65d14799
Frank <fshih37@gmail.com>
2024-12-05 03:53:07 -0500

    update

--- PINN/models/poisson.py
+++ PINN/models/poisson.py
@@ -138,6 +138,11 @@ class PoissonCallback(BaseCallback):
         true_Z = sol_y - true_y
         
         latent_Z = torch.cat([ Z for Z in self.model.latent_Z if Z is not None], dim=0).flatten().detach().cpu().numpy()
+        
+        np.save(os.path.join(self.save_path, 'true_Z.npy'), true_Z)
+        np.save(os.path.join(self.save_path, 'latent_Z.npy'), latent_Z)
+        
+        plt.subplots(figsize=(6, 6))
         plt.scatter(true_Z, latent_Z, label='Latent Z')
         plt.xlabel('True Z')
         plt.ylabel('Latent Z')
