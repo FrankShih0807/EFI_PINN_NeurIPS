177e0f63d0dbf978901b42022f76ffe88be1cde0
Frank Shih <fshih37@gmail.com>
2025-01-06 11:45:59 -0500

    update

--- summary.py
+++ summary.py
@@ -325,7 +325,8 @@ progress_df = collect_progress_data(output_dir)
 df = progress_df[progress_df['train/progress']==1.0]
 df = df.loc[:, ~df.columns.str.startswith('train')]
 df.rename(columns=lambda x: x.split('/')[-1], inplace=True)
-print(df[(df['mse']>0.01) & (df['model']=='poisson-inverse') ])
+print(df[(df['mse']>0.001) & (df['model']=='poisson-inverse') ])
+# print(df[(df['k_coverage_rate']<1.0) & (df['algo']=='efi_size80_theta2_2') ])
 
 df = df[['model', 'algo', 'mse', 'coverage_rate', 'ci_range', 'k_mean', 'k_coverage_rate', 'k_ci_range']]
 # df = df.dropna()
