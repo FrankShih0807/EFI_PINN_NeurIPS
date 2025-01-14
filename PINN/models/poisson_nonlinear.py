6965d2bc6f42e8bf8aa02e1a2475f3e1ffe09473
Frank <fshih37@gmail.com>
2025-01-14 11:47:10 -0500

    update

--- PINN/models/poisson_nonlinear.py
+++ PINN/models/poisson_nonlinear.py
@@ -143,7 +143,7 @@ class PoissonNonlinearCallback(BaseCallback):
         pred_y = self.model.net(self.eval_X).detach().cpu()
         self.eval_buffer.add(pred_y)
         if self.physics_model.is_inverse:
-            self.k_buffer.add(self.model.pe_variables.detach().numpy()[0])
+            self.k_buffer.add(self.model.pe_variables[0].item())
         # print(len(self.eval_buffer))
         
     
