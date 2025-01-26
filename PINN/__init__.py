e89d9db6cbd4d689b22887ffc6a9e76b5e8addc4
Zhenghao Jiang <hzyhjzh@gmail.com>
2025-01-26 15:18:04 -0500

    move to archive

--- PINN/__init__.py
+++ PINN/__init__.py
@@ -3,5 +3,5 @@ from PINN.pinn import PINN
 # from PINN.pretrain_efi import Pretrain_EFI
 from PINN.pinn_efi import PINN_EFI
 from PINN.bpinn_hmc import BayesianPINN
-from PINN.bpinn_hmc_inv import BayesianPINN_Inverse
+from PINN.bpinn import BayesianPINN_Inverse
 from PINN.pinn_inverse import PINN_Inverse
